8be64893b9b714b8c126eed17f6193671094f4b1
Matthew <matteidsen@gmail.com>
2025-01-17 16:01:16 -0800

    fix player score, forgot that ++ doesn't work in javascript

--- app.js
+++ app.js
@@ -2,41 +2,43 @@ let computerScore = 0;
 let humanScore = 0;
 
 function playRound(humanChoice, compChoice){
+    console.log(`Player chose ${humanChoice}`);
+    console.log(`Computer chose ${computerChoice}`);
     if(humanChoice === "rock") {
         if(compChoice === "scissors"){
             console.log("You win! Rock beats scissors!")
-            humanScore++;
+            humanScore+=1;
         } else if(compChoice === "paper") {
             console.log("You lose! Paper beats rock!");
-            computerScore++;
+            computerScore+=1;
         } else {
             console.log("It's a tie!");
         }
     } else if(humanChoice === "paper"){
         if(compChoice === "rock"){
             console.log("You win! Paper beats rock!");
-            humanScore++;
+            humanScore+=1;
         } else if(compChoice === "scissors"){
             console.log("You lose! Scissors beats paper!");
-            computerScore++;
+            computerScore+=1;
         } else {
             console.log("It's a tie!");
         } 
     } else if(humanChoice === "scissors"){
         if(compChoice === "paper"){
             console.log("You win! Scissors beats paper!");
-            humanScore++;
+            humanScore+=1;
         } else if(compChoice === "rock"){
             console.log("You lose! Rock beats scissors!");
-            computerScore++;
+            computerScore+=1;
         } else {
             console.log("It's a tie!");
         }
     } else {
         console.log("Something went wrong.");
     }
-    console.log(humanChoice);
-    console.log(computerChoice);
+    console.log(`Player score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`)
 }
 
 function getComputerChoice(){
