0133db40473888f337d93a8afd13a1dc0c216f53
Matthew <matteidsen@gmail.com>
2025-01-17 16:18:47 -0800

    add full game functionality

--- app.js
+++ app.js
@@ -1,46 +1,3 @@
-let computerScore = 0;
-let humanScore = 0;
-
-function playRound(humanChoice, compChoice){
-    console.log(`Player chose ${humanChoice}`);
-    console.log(`Computer chose ${computerChoice}`);
-    if(humanChoice === "rock") {
-        if(compChoice === "scissors"){
-            console.log("You win! Rock beats scissors!")
-            humanScore+=1;
-        } else if(compChoice === "paper") {
-            console.log("You lose! Paper beats rock!");
-            computerScore+=1;
-        } else {
-            console.log("It's a tie!");
-        }
-    } else if(humanChoice === "paper"){
-        if(compChoice === "rock"){
-            console.log("You win! Paper beats rock!");
-            humanScore+=1;
-        } else if(compChoice === "scissors"){
-            console.log("You lose! Scissors beats paper!");
-            computerScore+=1;
-        } else {
-            console.log("It's a tie!");
-        } 
-    } else if(humanChoice === "scissors"){
-        if(compChoice === "paper"){
-            console.log("You win! Scissors beats paper!");
-            humanScore+=1;
-        } else if(compChoice === "rock"){
-            console.log("You lose! Rock beats scissors!");
-            computerScore+=1;
-        } else {
-            console.log("It's a tie!");
-        }
-    } else {
-        console.log("Something went wrong.");
-    }
-    console.log(`Player score: ${humanScore}`);
-    console.log(`Computer score: ${computerScore}`)
-}
-
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random()*3+1);
     switch (compChoice){
@@ -76,7 +33,63 @@ function getHumanChoice() {
     }
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0; 
 
-playRound(humanChoice, computerChoice);
+    for(let i = 0; i<5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    
+    if(humanScore > computerScore){
+        console.log(`Player wins! Final score: ${humanScore}:${computerScore}!`)
+    } else if(computerScore > humanScore){
+        console.log(`Computer wins! Final score: ${computerScore}:${humanScore}!`)
+    } else {
+        console.log("The final score is a tie!");
+    }
+
+    function playRound(humanChoice, compChoice){
+        console.log(`Player chose ${humanChoice}`);
+        console.log(`Computer chose ${compChoice}`);
+        if(humanChoice === "rock") {
+            if(compChoice === "scissors"){
+                console.log("You win! Rock beats scissors!")
+                humanScore+=1;
+            } else if(compChoice === "paper") {
+                console.log("You lose! Paper beats rock!");
+                computerScore+=1;
+            } else {
+                console.log("It's a tie!");
+            }
+        } else if(humanChoice === "paper"){
+            if(compChoice === "rock"){
+                console.log("You win! Paper beats rock!");
+                humanScore+=1;
+            } else if(compChoice === "scissors"){
+                console.log("You lose! Scissors beats paper!");
+                computerScore+=1;
+            } else {
+                console.log("It's a tie!");
+            } 
+        } else if(humanChoice === "scissors"){
+            if(compChoice === "paper"){
+                console.log("You win! Scissors beats paper!");
+                humanScore+=1;
+            } else if(compChoice === "rock"){
+                console.log("You lose! Rock beats scissors!");
+                computerScore+=1;
+            } else {
+                console.log("It's a tie!");
+            }
+        } else {
+            console.log("Something went wrong.");
+        }
+        console.log(`Player score: ${humanScore}`);
+        console.log(`Computer score: ${computerScore}`)
+    }
+}
+
+playGame();
